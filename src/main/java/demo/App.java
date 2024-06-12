7717e2095d06e5bbf2f2d6bf76445d486423276e
MySecondAttemptToFetchDataFromRemoteToLocal <surajsahu2807@gmail.com>
2024-06-13 01:16:58 +0530

    TestCase1 & 2 Code Base

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -13,11 +13,14 @@ public class App {
         //TODO: call your test case functions one after other here
 
         tests.testCase01();
+        tests.testCase02();
+        // tests.testCase03();
+        // tests.testCase04();
 
         //END Tests
 
 
-        tests.endTest(); // End your test by clearning connections and closing browser
+        // tests.endTest(); // End your test by clearning connections and closing browser
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,18 +1,24 @@
 package demo;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.Select;
+
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class TestCases {
     ChromeDriver driver;
+    
     public TestCases()
     {
         System.out.println("Constructor: TestCases");
@@ -35,7 +41,7 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         // Set browser to maximize and wait
-        driver.manage().window().maximize();
+        // driver.manage().window().maximize();
     }
 
     public void endTest()
@@ -49,8 +55,37 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.get("https://calendar.google.com/calendar/u/0/r?pli=1");
+        String Currenturl = driver.getCurrentUrl();
+        if(Currenturl.contains("calendar")){
+            System.out.println("The URL of the Calendar homepage contains \"calendar\"");
+        }else{
+            System.out.println("The URL of the Calendar homepage doesn't contains \"calendar\".");
+        }
+        System.out.println("end Test case: testCase01");
+    }
+
+    public void testCase02() throws InterruptedException{
+        try{
+            System.out.println("Start Test case: testCase02");
+            WebElement DropDownBox = driver.findElement(By.xpath("//span[text()='Day' and @class='VfPpkd-vQzf8d']"));
+            DropDownBox.click();
+            WebElement MonthViewOpt = driver.findElement(By.xpath("//span[text()='Month']"));
+            MonthViewOpt.click();
+            // WebElement TodayDate = driver.findElement(By.xpath("//h2[@class='w48V4c ubOFEd F262Ye']//parent::div"));
+            // TodayDate.click();
+            String CurrentDate = "13";
+            List<WebElement> Alldates = driver.findElements(By.xpath("//h2[@class='w48V4c ubOFEd']"));
+            for(WebElement e : Alldates){
+                if(e.getText().equals(CurrentDate)){
+                    e.click();
+                    Thread.sleep(2000);
+                }
+            }
+            System.out.println("end Test case: testCase02");
+        }catch(Exception e){
+            e.printStackTrace();
+        }   
     }
 
 
